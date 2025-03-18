68281997f9620c4c0a887e7daac363a883d5ed94
João Figueredo <joaoklvf@gmail.com>
2025-03-18 07:54:11 -0300

    feat: infra crosscutting bus layer

---
+++ SimpleBlog.Infra.CrossCutting.Bus/EventBus.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SimpleBlog.Domain.Interfaces;
+
+namespace SimpleBlog.Infra.CrossCutting.Bus;
+
+public class EventBus(IMediator mediator) : IEventBus
+{
+    private readonly IMediator _mediator = mediator;
+
+    public Task Publish<T>(T command) where T : INotification =>
+        _mediator.Publish(command);
+
+    public Task<K> Send<T, K>(T command) where T : IRequest<K> =>
+        _mediator.Send(command);
+}
